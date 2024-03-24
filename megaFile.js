dac0eea5dd54451b4e9e415be411c3ffda8e610c
KaiHikichi <kai.hikichi@gmail.com>
2024-03-23 20:48:06 -0700

    straight

--- megaFile.js
+++ megaFile.js
@@ -1,8 +1,33 @@
-numbers = [2,1,0,1,0,1,0,1,1,0,1,0,0]; //2,3,4,5,6,7,8,9,x,j,q,k,a
+numbers = [1,1,0,1,0,1,0,0,0,0,1,1,2]; //2,3,4,5,6,7,8,9,x,j,q,k,a
 theSuits = [1,2,3,1]; // s, c, h, d
 
-a = pair(numbers, theSuits);
+a = twoPair(numbers);
 console.log(a);
+
+
+function straight(nums){
+    counter = 0;
+    highestCard;
+    searching = true;
+    for (var i = nums.length - 1; i >= 0; i--){
+        if(counter == 5){
+            return [true, 1000*(5*highestCard -10)];
+        }
+
+        if(nums[i] != 0){
+            if(searching == true){
+                highestCard = (i+2);
+                searching = false;
+            }
+            counter++;
+        }
+        else{
+            counter = 0;
+            searching = true;
+        }
+    }
+    return [false, NaN];
+}
 
 function twoPair(nums){
     counter = 0;
